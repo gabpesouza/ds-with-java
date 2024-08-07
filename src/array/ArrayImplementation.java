a99ac76ac84346a55adf3c9d3036b84061d3b9d1
Gabriel Pereira <gabriel.psouza1997@outlook.com>
2024-08-06 23:02:31 -0300

    Basic unordered array implementation

---
+++ src/array/ArrayImplementation.java
@@ -0,0 +1,60 @@
+package array;
+
+public class ArrayImplementation {
+
+    private Long[] array;
+    private int size;
+
+    public ArrayImplementation(int maxSize) {
+        array = new Long[maxSize];
+        size = 0;
+    }
+
+    public void insert(long value) {
+        this.array[size] = value;
+        size++;
+    }
+
+    public boolean find(long searchedValue) {
+
+        for (long currentValue : array) {
+            if (currentValue == searchedValue) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean delete(long value) {
+
+        int j;
+        boolean found = false;
+
+        for (j = 0; j < size; j++) {
+            if(value == array[j]) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            return false;
+        } else {
+            for (int k = j; k < size; k++) {
+                array[k] = array[k + 1];
+            }
+            size--;
+            return true;
+        }
+    }
+
+    public void display() {
+
+        for (Long value : array) {
+            System.out.print(value + " ");
+        }
+    }
+
+
+}
---
+++ src/array/Main.java
@@ -0,0 +1,23 @@
+package array;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        ArrayImplementation arrayImpl = new ArrayImplementation(10);
+
+        arrayImpl.insert(10);
+        arrayImpl.insert(15);
+        arrayImpl.insert(20);
+        arrayImpl.insert(32);
+        arrayImpl.insert(22);
+        arrayImpl.insert(24);
+        arrayImpl.insert(44);
+
+        System.out.println(arrayImpl.find(44));
+
+        System.out.println(arrayImpl.delete(44));
+
+        arrayImpl.display();
+    }
+}
